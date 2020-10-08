61b11db031980215c34d982078f73b7ac1d5dd5b
Penghua <kids5346@gmail.com>
2020-10-08 10:13:44 +0800

    feat: set min-width for page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from './components/Header';
+
+const AppDiv = styled.div`
+  min-width: 1200px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <AppDiv>
+    </AppDiv>
   );
 }
 
