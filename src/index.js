fe3330fd5fc211d90a6fbfa5e3a8db439e42dcea
Penghua <kids5346@gmail.com>
2020-10-10 16:31:21 +0800

    build: redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { store } from './store/index';
 import * as serviceWorker from './serviceWorker';
 import { createGlobalStyle } from 'styled-components';
 
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+const initialState = {
+  list: [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
+};
+
+export const rootReducer = (state = initialState, action) => {
+  return state;
+};
